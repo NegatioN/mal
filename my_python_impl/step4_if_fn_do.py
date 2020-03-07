b36fb5c7f4e0f461e4610730c86bd6b3ea76b6d0
NegatioN <joakimrishaug@gmail.com>
2020-03-07 16:09:08 +0100

    110 working

--- my_python_impl/step4_if_fn_do.py
+++ my_python_impl/step4_if_fn_do.py
@@ -1,16 +1,13 @@
-from functools import reduce
-import operator
-from mal_types import SpecialSymbol, Symbol, _specialsymbol_like
+from mal_types import String, Symbol, _specialsymbol_like, Function, Int
 
 from reader import read_str
 from printer import pr_str
 from env import Env
+from mal_types import Nil
+from core import ns
 
 repl_env = Env(None)
-repl_env.update({'+': lambda *x: reduce(operator.add, x),
-                 '-': lambda *x: reduce(operator.sub, x),
-                 '*': lambda *x: reduce(operator.mul, x),
-                 '/': lambda *x: reduce(operator.truediv, x)})
+repl_env.update(ns)
 
 
 def eval_ast(ast, repl_env):
@@ -30,11 +27,30 @@ def eval_ast(ast, repl_env):
 def EVAL(ast, repl_env):
     if isinstance(ast, list):
         if len(ast) > 0:
-            if _specialsymbol_like(ast[0], 'def!'):
+            arg1 = ast[0]
+            if _specialsymbol_like(arg1, 'do'):
+                return eval_ast(ast[1:], repl_env)[-1]
+
+            elif _specialsymbol_like(arg1, 'if'):
+                o = EVAL(ast[1], repl_env)
+                # Bool truth in Python is 1, but 0 is expected.
+                if isinstance(o, Int):
+                    o = not bool(o)
+                # empty types count as true values, contrary to Python
+                if o == '' or o == []:
+                    o = True
+
+
+                if not isinstance(o, Nil) and o:
+                    return EVAL(ast[2], repl_env)
+                return EVAL(ast[3], repl_env) if len(ast) >= 4 else Nil
+
+            elif _specialsymbol_like(arg1, 'def!'):
                 evaled = EVAL(ast[2], repl_env)
                 repl_env.set(ast[1], evaled)
                 return evaled
-            elif _specialsymbol_like(ast[0], 'let*'):
+
+            elif _specialsymbol_like(arg1, 'let*'):
                 e = Env(repl_env)
                 counter, let_vals = 0, ast[1]
                 while counter < len(let_vals) - 1:
@@ -44,7 +60,11 @@ def EVAL(ast, repl_env):
 
                 return EVAL(ast[2], e)
 
-
+            elif _specialsymbol_like(arg1, 'fn*'):
+                def closure(*inp):
+                    sub_env = Env(outer=repl_env, binds=ast[1], exprs=inp)
+                    return EVAL(ast[2], sub_env)
+                return Function(closure)
 
             args = eval_ast(ast, repl_env)
             f = args[0]
